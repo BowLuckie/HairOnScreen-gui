d4ab3a303cb8e636b9cd5bcf5a2c76423f49b07c
BowLuckie <bowluckie@gmail.com>
2026-06-15 23:25:16 +1000

    added more context menu options to gui

--- background.js
+++ background.js
@@ -1,39 +1,41 @@
 // generate the context menus brought up by clicking the browser action
-chrome.storage.sync.get("numHairImages", function(items) {
-  var hairCount = items.hasOwnProperty("numHairImages") ? items.numHairImages : 1;
-  
+chrome.storage.sync.get("numHairImages", function (items) {
+  var hairCount = items.hasOwnProperty("numHairImages")
+    ? items.numHairImages
+    : 1;
+
   chrome.contextMenus.removeAll();
   chrome.contextMenus.create({
     title: hairCountText(hairCount),
     id: "hair_count",
     contexts: ["action"],
-    enabled: false
+    enabled: false,
   });
   chrome.contextMenus.create({
     title: "More hair!",
     id: "more_hair",
-    contexts: ["action"]
+    contexts: ["action"],
   });
   chrome.contextMenus.create({
     title: "Less hair!",
     id: "less_hair",
-    contexts: ["action"]
+    contexts: ["action"],
   });
   chrome.contextMenus.create({
     title: "Reset hair!",
     id: "reset_hair",
-    contexts: ["action"]
+    contexts: ["action"],
   });
   chrome.contextMenus.create({
     title: "Randomize hair!",
     id: "randomize_hair",
-    contexts: ["action"]
+    contexts: ["action"],
   });
   chrome.contextMenus.create({
     title: "(reload after updating)",
     id: "reload_prompt",
     contexts: ["action"],
-    enabled: false
+    enabled: false,
   });
 });
 
@@ -52,47 +54,73 @@ function hairCountText(num) {
 
 // general function for updating the hair count
 function updateNumImages(updateFunction) {
-  chrome.storage.sync.get("numHairImages", function(items) {
+  chrome.storage.sync.get("numHairImages", function (items) {
     var numHairImages;
     if (items.hasOwnProperty("numHairImages")) {
       numHairImages = updateFunction(items.numHairImages);
-    }
-    else {
+    } else {
       numHairImages = updateFunction(1);
     }
-    
-    chrome.storage.sync.set( {"numHairImages": numHairImages } );
-    
-    chrome.contextMenus.update("hair_count", { title: hairCountText(numHairImages) });
+
+    chrome.storage.sync.set({ numHairImages: numHairImages });
+
+    chrome.contextMenus.update("hair_count", {
+      title: hairCountText(numHairImages),
+    });
   });
 }
 
 // increment count
 function incrementNumImages() {
-  updateNumImages(function(num) {
+  updateNumImages(function (num) {
     return num + 1;
   });
 }
 
 // decrement count
 function decrementNumImages() {
-  updateNumImages(function(num) {
-    return Math.max(0, num-1);
+  updateNumImages(function (num) {
+    return Math.max(0, num - 1);
   });
 }
 
 // reset count to 1
 function resetNumImages() {
-  updateNumImages( function() { return 1; } );
+  updateNumImages(function () {
+    return 1;
+  });
 }
 
 // randomize positions
 function randomizeHair() {
-  chrome.storage.sync.set( { "hairImages": [] } );
+  chrome.storage.sync.set({ hairImages: [] });
 }
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if ( message.action == "popupCallIncrement" ) {
-    incrementNumImages();
+// connect popup and background
+chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+  let action = message.action;
+
+  if (action == "getNumImages") {
+    chrome.storage.sync.get("numHairImages", (items) => {
+      sendResponse({ numImages: items.numHairImages ?? 1 });
+    });
+    return true;
   }
-})
+
+  if (action == "popupCallIncrement") {
+    incrementNumImages();
+    sendResponse({});
+  } else if (action == "popupCallDecrement") {
+    decrementNumImages();
+    sendResponse({});
+  } else if (action == "popupCallReset") {
+    resetNumImages();
+    sendResponse({});
+  } else if (action == "popupCallRandomize") {
+    randomizeHair();
+    sendResponse({});
+  } else if (action == "popupCallSetExact") {
+    updateNumImages(() => message.value);
+    sendResponse({});
+  }
+});
--- popup.js
+++ popup.js
@@ -1,5 +1,91 @@
-document.getElementById("increment").addEventListener("click", () => {
-  chrome.runtime.sendMessage({
-    action: "popupCallIncrement"
-  })
+document.addEventListener("DOMContentLoaded", () => {
+  main();
 });
+
+function main() {
+  update_count(true);
+  document.getElementById("increment").addEventListener("click", () => {
+    chrome.runtime.sendMessage(
+      {
+        action: "popupCallIncrement",
+      },
+      () => {
+        update_count();
+      },
+    );
+  });
+
+  document.getElementById("decrement").addEventListener("click", () => {
+    chrome.runtime.sendMessage(
+      {
+        action: "popupCallDecrement",
+      },
+      () => {
+        update_count();
+      },
+    );
+  });
+
+  document.getElementById("randomize").addEventListener("click", () => {
+    chrome.runtime.sendMessage(
+      {
+        action: "popupCallRandomize",
+      },
+      () => {
+        needs_reset();
+      },
+    );
+  });
+
+  document.getElementById("reset").addEventListener("click", () => {
+    chrome.runtime.sendMessage(
+      {
+        action: "popupCallReset",
+      },
+      () => {
+        update_count();
+      },
+    );
+  });
+
+  document.getElementById("countRequest").addEventListener("click", () => {
+    chrome.runtime.sendMessage({ action: "getNumImages" }, (response) => {
+      console.log(response.numImages);
+    });
+  });
+
+  document.getElementById("exact").addEventListener("click", () => {
+    const value = parseInt(document.getElementById("exactIn").value, 10);
+    if (value >= 0 && value <= 2048) {
+      chrome.runtime.sendMessage(
+        { action: "popupCallSetExact", value: value },
+        () => {
+          update_count();
+        },
+      );
+    } else {
+      console.log("out of bounds");
+
+      let warning_text = document.getElementById("needsReset");
+      warning_text.textContent = "hair amount must be between 0 and 2048!";
+    }
+  });
+}
+
+function update_count(offrip = false) {
+  let counter = document.getElementById("counter");
+
+  chrome.runtime.sendMessage({ action: "getNumImages" }, (response) => {
+    counter.textContent = response.numImages;
+  });
+
+  if (!offrip) {
+    needs_reset();
+  }
+}
+
+function needs_reset() {
+  let warning_text = document.getElementById("needsReset");
+
+  warning_text.textContent = "Webpage must be reloaded for hair to be visible!";
+}
